c84dec2b84cf61ff4e7e9c0737f13fabe63d9bbc
agumy <agumy@agumynoMacBook-Air.local>
2021-02-05 23:39:04 +0900

    fix style

--- components/organisms/DayCalendar.tsx
+++ components/organisms/DayCalendar.tsx
@@ -3,13 +3,21 @@ export const DayCalendar = () => {
     <div className="h-full flex flex-col px-3">
       <div className="h-1/6"></div>
       <div className="flex flex-col h-5/6 border-t-2">
-        <div className="overflow-auto">
+        <div className="flex flex-col w-full overflow-auto">
           {[...Array(23).keys()].map((i) => (
             <div
               key={i}
               className="flex items-center border-t-2 first:border-t-0 h-16"
             >
-              {i}
+              <div
+                style={{
+                  flex: '0 0 4rem',
+                }}
+                className="flex items-center justify-center pl w-16 border-r-2 h-full"
+              >
+                {i + 1}
+              </div>
+              <div className="flex items-center flex-auto h-full"></div>
             </div>
           ))}
         </div>
